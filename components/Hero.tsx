f0d59f8e880b8dc68cd36949bc3110693d798bf6
Ricardo Cruz <130194380+ricardocrzz@users.noreply.github.com>
2023-09-28 17:41:06 -0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ function Hero({ }: Props) {
             <div className='z-20'>
                 <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
                 <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
-                    <span className='mr-3'>Hi, I'm {text}</span>
+                    <span className='mr-3'>Hi, I am {text}</span>
                     <Cursor cursorColor='#F7AB0A' />
                 </h1>
                 <div className="pt-5">
