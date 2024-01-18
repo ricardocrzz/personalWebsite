6988e50b96717a042bce0612f4aca98864fa0061
ricardocrzz <ricky1301@hotmail.com>
2024-01-18 02:27:00 -0700

    changed the src of ricardoShot.png

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ function Home() {
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
             <Image
-              src="/ricardoShot.png"
+              src="ricardoShot.png"
               alt="Ricardo's Photo"
               className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
               width={100}
