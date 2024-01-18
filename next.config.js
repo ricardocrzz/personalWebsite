49df983a4bee472824ee311eba4657ec604cce5c
ricardocrzz <ricky1301@hotmail.com>
2024-01-18 02:17:30 -0700

    changed line 20 in plcomp

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
 // next.config.js
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['mir-s3-cdn-cf.behance.net', 'content.linkedin.com', "www.online-tech-tips.com", "nbcsports.brightspotcdn.com"],
+    domains: ['mir-s3-cdn-cf.behance.net', 'content.linkedin.com', 'www.online-tech-tips.com', 'nbcsports.brightspotcdn.com'],
+    unoptimized: true,
   },
 };
